f4d14e10249255444c2b94d52673a2a1d23949bf
makyol <mehmetali.akyol@arcelik.com>
2024-05-22 05:33:50 +0300

    Bump the version.

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@
 # -- Project information -----------------------------------------------------
 project = 'Land Use Mix'
 author = 'Mehmet Ali Akyol'
-release = '0.0.7'
+release = '0.0.8'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
