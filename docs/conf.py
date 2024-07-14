d047cb9c7e4124e70f5bc5407ff953f5eb6b7070
makyol <mehmetali.akyol@gmail.com>
2024-07-14 14:44:31 +0300

    Python docstrings are added.

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,8 @@
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../landusemix'))
+
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information -----------------------------------------------------
